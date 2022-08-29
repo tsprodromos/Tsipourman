a117a8e24b894cedbc60149581c116743ecb6d3e
tsprodromos <tsprodromos@gmail.com>
2022-08-29 14:00:49 +0300

    welcome page changes

---
+++ app/src/main/java/com/example/tsipourman/MainActivity2.java
@@ -0,0 +1,18 @@
+package com.example.tsipourman;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+
+
+public class MainActivity2 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+}}
